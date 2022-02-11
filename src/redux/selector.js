7c357f6ae630dd49e88c584a63e478c7f8ae20a3
dunghoang97 <dunghoang.5397@gmail.com>
2022-02-11 09:09:15 +0700

    changing filter

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -8,8 +8,8 @@ export const priorityFilterChange = (state) => state.filters.priorities
 export const todosRemainingSelector = createSelector(todoListSelector, statusFilterSelector, searchTextSelector, priorityFilterChange,(todoList, status, searchText, priorities) => {
     return todoList.filter((todo) => {
         if(status === 'All') {
-            return priorities.length ? todo.name.includes(searchText) && priorities.includes(todo.priority) : todo.name.includes(searchText)
+            return priorities.length ? todo.name.toLowerCase().includes(searchText.toLowerCase()) && priorities.includes(todo.priority) : todo.name.toLowerCase().includes(searchText.toLowerCase())
         }
-        return todo.name.includes(searchText) && (status === 'Completed' ? todo.completed : !todo.completed) && (priorities.length ? priorities.includes(todo.priority) : true)
+        return todo.name.toLowerCase().includes(searchText.toLowerCase()) && (status === 'Completed' ? todo.completed : !todo.completed) && (priorities.length ? priorities.includes(todo.priority) : true)
     })
 } )
